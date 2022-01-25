e1f9ccec61fad2d81b87122dfea71fd2df7741ab
mwoolf87 <michael.j.woolf@gmail.com>
2022-01-24 19:04:09 -0500

    renamed files to work with Navbar imports

--- src/components/userForm.jsx
+++ src/components/userForm.jsx
@@ -1,44 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { saveUserData } from "../actions/userData-actions";
 
 const UserForm = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-    const sendUserData = (e) => {
-        e.preventDefault();
-        const userData = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            phoneNumber: phoneNumber
-        }
-        saveUserData(dispatch, userData);
-        navigate('/jokes');
-    }
+  const sendUserData = e => {
+    e.preventDefault();
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber
+    };
+    saveUserData(dispatch, userData);
+    navigate("/jokes");
+  };
 
-    return (
-        <div>
-            <form onSubmit={(e) => sendUserData(e)}>
-                <label htmlFor="firstName">Name</label>
-                <input type="text" placeholder="John" name="firstName" onChange={(e) => setFirstName(e.target.value)}/>
-                <label htmlFor="lastName">Name</label>
-                <input type="text" placeholder="Smith" name="lastName" onChange={(e) => setLastName(e.target.value)}/>
-                <label htmlFor="email">Email</label>
-                <input type="text" placeholder="johnsmith@gmail.com" name="email" onChange={(e) => setEmail(e.target.value)}/>
-                <label htmlFor="name">Phone Number</label>
-                <input type="text" placeholder="(000)-000-0000" name="phone" onChange={(e) => setPhoneNumber(e.target.value)}/>
-                <button type="submit">Next Step</button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div>
+      <form onSubmit={e => sendUserData(e)}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          placeholder="John"
+          name="firstName"
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          placeholder="Smith"
+          name="lastName"
+          onChange={e => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          placeholder="johnsmith@gmail.com"
+          name="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="name">Phone Number</label>
+        <input
+          type="text"
+          placeholder="(000)-000-0000"
+          name="phone"
+          onChange={e => setPhoneNumber(e.target.value)}
+        />
+        <button type="submit">Next Step</button>
+      </form>
+    </div>
+  );
+};
 
 export default UserForm;
